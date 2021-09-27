8b6f646909219fbf4e6a33d9e46f970960ed1b19
Monddd <raymond02joseph@gmail.com>
2021-09-27 16:34:55 +0800

    Final

--- client/src/index.js
+++ client/src/index.js
@@ -5,7 +5,7 @@ import App from './App';
 import { BrowserRouter } from 'react-router-dom';
 import React from 'react';
 import ReactDOM from 'react-dom';
-import reportWebVitals from './reportWebVitals';
+import reportWebVitals from './reportWebVitals'
 
 ReactDOM.render(
   <React.StrictMode>
